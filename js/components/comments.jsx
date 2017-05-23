b623f941bf3f65b92879d2b47d0bc8163c8e5198
Matt Huntington <matt.huntington@gmail.com>
2017-05-22 22:14:59 -0400

    redux working

--- js/components/comments.jsx
+++ js/components/comments.jsx
@@ -3,23 +3,10 @@ import CommentsList from './commentslist.jsx'
 import CommentsForm from './commentsform.jsx'
 
 class Comments extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            comments: []
-        }
-        this.addComment = this.addComment.bind(this);
-    }
-    addComment(value){
-        this.state.comments.push(value); //this alone won't notify React to update the DOM
-        this.setState({
-            comments: this.state.comments
-        });
-    }
     render(){
         return <section>
-            <CommentsList comments={this.state.comments}></CommentsList>
-            <CommentsForm handleSubmit={this.addComment}></CommentsForm>
+            <CommentsList></CommentsList>
+            <CommentsForm></CommentsForm>
         </section>
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Comments from './components/comments.jsx';
+import { Provider } from 'react-redux';
+import store from './store.js';
 
 ReactDOM.render(
-    <Comments></Comments>,
+    <Provider store={store}>
+        <Comments></Comments>
+    </Provider>,
     document.querySelector('main')
 );
